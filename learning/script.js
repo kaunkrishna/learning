e2b1da842300a44e7e43da04c7a6cba291672592
KAUN <kaunkrishna@proton.me>
2026-08-11 15:24:59 +0530

    Events, Event Bubbling, setInterval & setTimeout

--- learning/script.js
+++ learning/script.js
@@ -1,22 +1,12 @@
 console.log("hello world");
 
-// let boxes = document.getElementsByClassName("box")
-// console.log(boxes)
+let btn = document.getElementById("btn")
 
-// boxes[2].style.backgroundColor = "red";
+btn.addEventListener("dblclick", ()=>{
+    // document.querySelector(".box").innerHTML = "Hello I am a boxer"
+    alert("hands up! button molestor")
+})
 
-// document.getElementById("red").style.backgroundColor = "red";
-
-// document.querySelector(".box").style.backgroundColor = "green";
-
-// document.querySelectorAll(".box").forEach((e) => {
-//   e.style.backgroundColor = "blue";
-// });
-
-// let div = document.createElement("div");
-// div.innerHTML = "i have been created by <b>KAUN<b>";
-// div.setAttribute("class", "created");
-// document.querySelector(".container").append(div);
-
-let cont = document.querySelector(".container")
-cont.insertAdjacentHTML("afterend", "<b>Hello I'm under the water plz help me, here to much raining........</b>")
+document.addEventListener("contextmenu", () => {
+    alert("really wanna see the inner me?");
+});
